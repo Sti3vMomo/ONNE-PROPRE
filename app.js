89379721a760f182aaa2d0700ea2adfc6ea6df42
Sti3vMomo <131660942+Sti3vMomo@users.noreply.github.com>
2023-11-06 10:12:36 +0100

    envoie tout cousin

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ const app = express();
 const port = 3000;
 
 app.use(express.static('public'))
+
 // Middleware pour gérer les requêtes JSON
 app.use(express.json());
 
@@ -11,12 +12,12 @@ app.use(express.urlencoded({ extended: true }));
 
 // Route pour la page d'accueil
 app.get('/', (req, res) => {
-  res.sendFile(__dirname + '  /public/index.html')
+  res.sendFile(__dirname + '/index.html')
 });
 
 // Route 404 (page non trouvée)
-app.use((req, res) => {
-  res.status(404).send('Page non trouvée');
+app.use((req, res, next) => {
+  res.status(404).sendFile(__dirname + '/public/html/404.html');
 });
 
 app.listen(port, () => {
